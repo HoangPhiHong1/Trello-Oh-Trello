07416a627020de85924e1e1dc331e7dd9cbe396d
hoangphihong1 <hphoang@knox.edu>
2024-01-06 12:47:15 -0600

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>Web 1 Web</div>
+      <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
       <Button variant='contained'>Hello World</Button> <br/>
       <Button variant='outlined'>Xin Chao</Button>  <br />
       <Button variant='text'>Chao Xin</Button>  <br />
