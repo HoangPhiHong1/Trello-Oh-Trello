1defeff0188f991c33a5312dcf93869ef441cffa
hoangphihong1 <hphoang@knox.edu>
2024-02-22 23:40:53 -0600

    init app bar p1.0

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,16 +1,32 @@
 import ModeSelect from '~/components/ModeSelect'
 import Box from '@mui/material/Box'
+import AppsIcon from '@mui/icons-material/Apps'
+import trelloLogo from '~/assets/mdi--trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import { ReactComponent as TrelloIcon } from '~/assets/mdi--trello.svg'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menus/Workspaces'
 
 function AppBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.light',
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect />
+      <Box sx={{ display: 'flex', alignItems:'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 2 }}>
+          <SvgIcon component={TrelloIcon} inheritViewBox sx = {{ color: 'primary.main' }} />
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
+        </Box>
+        <Workspaces />
+      </Box>
+      <Box>
+        <ModeSelect />
+      </Box>
     </Box>
   )
 }
