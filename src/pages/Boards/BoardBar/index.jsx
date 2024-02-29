7533f892496e96cabcf42692deec3a33ba8be7f2
HoangPhiHong1 <hphoang@knox.edu>
2024-02-28 19:57:57 -0600

    board bar p1

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,62 @@
+import DashboardIcon from '@mui/icons-material/Dashboard'
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: '2',
+      paddingX: '2',
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-            Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
+        <Chip
+          sx={{
+            color:'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root':{
+              color: 'primary.main'
+            },
+            '&:hover':{
+              bgcolor: 'primary.50'
+            }
+          }}
+          icon={<DashboardIcon />}
+          label="Team Alpha"
+          clickable
+        />
+        <Chip
+          sx={{
+            color:'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root':{
+              color: 'primary.main'
+            },
+            '&:hover':{
+              bgcolor: 'primary.50'
+            }
+          }}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
+
+      </Box>
     </Box>
   )
 }
